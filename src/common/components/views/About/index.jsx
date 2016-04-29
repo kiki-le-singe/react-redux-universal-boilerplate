1c2d34b83fdd4cacb1ca1c7556cc35900d90a9d3
kiki le singe <aalbertini95@gmail.com>
2016-04-29 13:04:13 +0200

    feat(About): add simple example using css module

--- src/common/components/views/About/index.jsx
+++ src/common/components/views/About/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import styles from './About.scss'
+
 export default function About() {
   return (
     <div className="page">
-      <div className="page-content">About</div>
+      <div className="page-content">
+        <h1 className={ styles.title }>About</h1>
+      </div>
     </div>
   )
 }
