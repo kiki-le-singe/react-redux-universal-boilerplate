9ea8c8b26d0f41273f5e2ee1c82c094cecfbeccd
kiki le singe <aalbertini95@gmail.com>
2016-04-29 17:58:36 +0200

    chore(webpack.prod): add webpack config for prod env

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,7 +1,114 @@
+import webpack from 'webpack'
 import _debug from 'debug'
+import WebpackIsomorphicToolsPlugin from 'webpack-isomorphic-tools/plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
+import isomorphicToolsConfig from './isomorphic.tools.config'
+import projectConfig, { paths } from '../config'
+
+const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(isomorphicToolsConfig)
 const debug = _debug('app:webpack:config:prod')
+const srcDir = paths('src')
+const nodeModulesDir = paths('nodeModules')
+const cssLoader = [
+  'css?modules',
+  'sourceMap',
+  'importLoaders=2',
+  'localIdentName=[name]__[local]___[hash:base64:5]'
+].join('&')
 
 debug('Create configuration.')
+const config = {
+  context: paths('base'),
+  devtool: 'source-ma',
+  entry: {
+    app: paths('entryApp'),
+    vendors: projectConfig.VENDOR_DEPENDENCIES
+  },
+  output: {
+    path: paths('dist'),
+    filename: '[name]-[hash].js'
+  },
+  resolve: {
+    root: [srcDir],
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel',
+        exclude: [nodeModulesDir],
+        include: [srcDir],
+        query: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: webpackIsomorphicToolsPlugin.regular_expression('styles'),
+        loader: ExtractTextPlugin.extract('style', `${cssLoader}!postcss!sass?sourceMap`)
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=fonts/[name].[ext]'
+      },
+      {
+        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+        loader: 'url?limit=10000'
+      }
+    ]
+  },
+  postcss: wPack => ([
+    require('postcss-import')({ addDependencyTo: wPack }),
+    require('postcss-url')(),
+    require('autoprefixer')({ browsers: ['last 2 versions'] })
+  ]),
+  plugins: [
+    new ExtractTextPlugin('[name].[contenthash].css', {
+      allChunks: true
+    }),
+    new webpack.DefinePlugin({
+      __CLIENT__: projectConfig.__CLIENT__,
+      __SERVER__: projectConfig.__SERVER__,
+      __DEV__: projectConfig.__DEV__,
+      __PROD__: projectConfig.__PROD__,
+      __DEBUG__: projectConfig.__DEBUG__
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendors', '[name].[hash].js'),
 
-// @TODO
+    // optimizations
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        sequences: true,
+        dead_code: true,
+        drop_debugger: true,
+        comparisons: true,
+        conditionals: true,
+        evaluate: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        if_return: true,
+        join_vars: true,
+        cascade: true,
+        drop_console: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+
+    webpackIsomorphicToolsPlugin
+  ]
+}
+
+export default config
