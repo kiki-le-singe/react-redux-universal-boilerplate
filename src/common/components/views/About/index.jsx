5d54ef4e66514fe87d770d9438aaceb39e69f0e6
kiki le singe <aalbertini95@gmail.com>
2016-05-29 23:02:39 +0200

    feat(About): implement common local styles

--- src/common/components/views/About/index.jsx
+++ src/common/components/views/About/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import classNames from 'classnames'
 
+import titleStyles from 'common/styles/local/title.css'
 import styles from './About.css'
 
 export default function About() {
   return (
     <div className="page">
       <div className="page-content">
-        <h1 className={styles.title}>About</h1>
+        <h1 className={classNames(styles.title, titleStyles.h1)}>About</h1>
       </div>
     </div>
   )
