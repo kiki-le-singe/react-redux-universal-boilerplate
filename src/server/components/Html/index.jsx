cd9a875284a60c5d19a44a70cba2009497776e40
kiki le singe <aalbertini95@gmail.com>
2016-06-25 18:01:20 +0200

    refactor(Html): update meta viewport tag

--- src/server/components/Html/index.jsx
+++ src/server/components/Html/index.jsx
@@ -44,7 +44,8 @@ export default class Html extends Component {
       <html>
         <head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1">
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <title>React Redux Universal Boilerplate</title>
