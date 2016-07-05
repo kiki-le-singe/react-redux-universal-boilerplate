7014498fa57027c4e867ac95c2a01eea019ca71e
kiki le singe <aalbertini95@gmail.com>
2016-07-05 16:00:55 +0200

    fix(Html): fix syntax error: Expected corresponding JSX closing tag for <meta>

--- src/server/components/Html/index.jsx
+++ src/server/components/Html/index.jsx
@@ -45,7 +45,7 @@ export default class Html extends Component {
         <head>
           <meta charSet="utf-8" />
 
-          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <title>React Redux Universal Boilerplate</title>
