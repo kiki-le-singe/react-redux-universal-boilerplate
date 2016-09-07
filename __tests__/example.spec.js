011321e89b75d0bd9e0fa48af5c0cb0b0b28fce9
kiki le singe <aalbertini95@gmail.com>
2016-09-07 21:18:04 +0200

    test(example.spec): add an example using enzyme

--- __tests__/example.spec.js
+++ __tests__/example.spec.js
@@ -1,3 +1,12 @@
+import React from 'react'
+import { mount, render, shallow } from 'enzyme'
+
+const Fixture = () => (
+  <div className="root top">
+    <span className="child bottom">test</span>
+  </div>
+)
+
 function hello(name, cb) {
   cb(`hello ${name}`)
 }
@@ -17,5 +26,21 @@ describe('hello', () => {
     hello('foo', cb)
 
     cb.should.have.been.calledWith('hello foo')
+  })
+})
+
+// Using of enzyme
+describe('Fixture', () => {
+  it('should have enzyme working', () => {
+    let wrapper = shallow(<Fixture />)
+    expect(wrapper.find('.root')).to.have.length(1)
+    expect(wrapper.find('.child')).to.have.length(1)
+
+    wrapper = mount(<Fixture />)
+    wrapper.setProps({ bar: 'foo' })
+    expect(wrapper.props().bar).to.equal('foo')
+
+    wrapper = render(<Fixture />)
+    expect(wrapper.text()).to.contain('test')
   })
 })
