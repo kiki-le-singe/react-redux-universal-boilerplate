32b64165f50b0ad463595e6b95f073b8a2aae713
kiki le singe <aalbertini95@gmail.com>
2016-09-08 22:13:50 +0200

    feat(HelloView): implement the Hello component

--- src/common/views/Hello/index.jsx
+++ src/common/views/Hello/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import Hello from 'common/components/Hello'
+
 export default function HelloView() {
   return (
     <div className="page page-hello">
-      <div className="page-content">Hello <span className="text">World!</span></div>
+      <div className="page-content">
+        <Hello name={<span className="text">World!</span>} />
+      </div>
     </div>
   )
 }
