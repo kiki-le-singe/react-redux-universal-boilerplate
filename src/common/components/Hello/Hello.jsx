d3d4a8e97365e9f7fe4e9f90c56261893236efb1
kiki le singe <aalbertini95@gmail.com>
2017-04-28 22:39:14 +0200

    refactor(proptypes): use PropTypes from `prop-types` instead of 'react'

--- src/common/components/Hello/Hello.jsx
+++ src/common/components/Hello/Hello.jsx
@@ -1,7 +1,8 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const propTypes = {
-  name: React.PropTypes.oneOfType([
+  name: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.element
   ])
--- src/common/views/CounterView/CounterView.jsx
+++ src/common/views/CounterView/CounterView.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 export default function CounterView({ counter, increment, decrement }) {
